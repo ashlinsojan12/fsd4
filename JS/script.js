856caeb46437ec6562d7d9ada3c4f64fb2e6504a
ashlinsojan12 <150226567+ashlinsojan12@users.noreply.github.com>
2024-05-21 15:21:05 +0530

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,32 @@
+
+// array
+// let fruits = ["Apple","Mango","orange",23]
+// console.log(fruits)
+// fruits.push("grapes");
+// console.log(fruits);
+// console.log(fruits.length);
+// fruits[2]="pineapple";
+// console.log(fruits);
+// fruits.unshift("pear");
+// console.log(fruits)
+// fruits.pop();
+// console.log(fruits);
+// fruits.shift();
+// console.log(fruits);
+
+// Array of object
+let bsc = [
+     {
+        fname:"christo",
+        age:18,
+     },
+     {
+        fname:"Anto",
+        age:10,
+     },
+     {
+        fname:"Elias",
+        age:19,
+     }
+    ];
+console.log(bsc[2].age);
